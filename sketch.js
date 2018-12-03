8ae7143cef67eae70f5b6447a9974df690e939d5
oliviabell1 <45546856+oliviabell1@users.noreply.github.com>
2018-12-03 01:34:59 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background('pink');
+  point(200, 200);        
+
+}
+
+function draw() {
+  //ellipse(mouseX, mouseY, 10, 10);
+  fill(200);
+  textSize(40);
+  textFont('Canterbury')
+  text("error404", mouseX, mouseY);
+  
+}
